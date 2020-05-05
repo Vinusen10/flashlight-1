63eff16fda5b46e73a877bcb429545af8fbe7c6b
Ghiri <ghiri@gmx.de>
2020-05-05 19:06:10 +0200

    refactor
    (cherry picked from commit df29bf28a74c029ff2704ebf5f73fd8730847277)

--- app/route/router.go
+++ app/route/router.go
@@ -1,12 +1,16 @@
 package route
 
 import (
+	"flashlight/app/controller"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func GetRouter() *mux.Router {
-
 	r := mux.NewRouter()
+	r.HandleFunc("/index", controller.Index).Methods("GET")
 
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css", http.FileServer(http.Dir("./assets/static/css"))))
+	r.PathPrefix("/img/").Handler(http.StripPrefix("/css", http.FileServer(http.Dir("./assets/static/ressource/img"))))
 	return r
 }
